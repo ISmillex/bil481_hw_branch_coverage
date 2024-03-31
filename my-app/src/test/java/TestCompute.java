67d9b1f1eaaed9ac551c7189f4140531be9e6c49
ISmillex <archynlegentey@gmail.com>
2024-03-31 13:03:20 +0300

    add testCountWhenQueueIsEmpty

--- my-app/src/test/java/TestCompute.java
+++ my-app/src/test/java/TestCompute.java
@@ -1,17 +1,23 @@
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestCompute {
   Compute c;
+  MessageQueue mq;
+
+  @Before
+  public void setup() {
+    mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+  }
 
   @Test
-  public void example() {
-    MessageQueue mq = mock(MessageQueue.class);
-    c = new Compute(mq);
-    assertTrue(true);
+  public void testCountWhenQueueIsEmpty() {
+    when(mq.size()).thenReturn(0);
+    assertEquals(-1, c.countNumberOfOccurrences("element"));
   }
+
 }
