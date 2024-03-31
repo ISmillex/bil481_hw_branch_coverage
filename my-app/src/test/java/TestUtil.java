28d99cd154b1a73863f2cf16045290cc96bf3858
ISmillex <archynlegentey@gmail.com>
2024-03-31 13:13:53 +0300

    add testComputeWithEvenNumberOfElements

--- my-app/src/test/java/TestUtil.java
+++ my-app/src/test/java/TestUtil.java
@@ -2,6 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class TestUtil {
   Util util;
@@ -19,6 +20,11 @@ public class TestUtil {
   @Test
   public void testComputeWithEvenNumberOfElements() {
     assertFalse(util.compute(1, 2));
+  }
+
+  @Test
+  public void testComputeWithOddNumberOfElementsNoDivisor() {
+    assertTrue(util.compute(1, 2, 3));
   }
 
 }
